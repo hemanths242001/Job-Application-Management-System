5530697e71a5d80eb2383b58bdde100b17c01f62
hemanths242001 <84668288+hemanths242001@users.noreply.github.com>
2025-07-31 11:44:46 +0530

    jobapp project

--- src/main/java/com/example/JobApplicationSystem/repository/JobRepository.java
+++ src/main/java/com/example/JobApplicationSystem/repository/JobRepository.java
@@ -2,7 +2,9 @@ package com.example.JobApplicationSystem.repository;
 
 import com.example.JobApplicationSystem.model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
 }
 
